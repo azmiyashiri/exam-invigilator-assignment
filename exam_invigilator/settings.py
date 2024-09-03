b56cba1b75880e8460bbf23b56734f2018d771c8
Ayeshath Azmiya shiri <162473054+azmiyashiri@users.noreply.github.com>
2024-09-03 20:05:43 +0530

    Update settings.py

--- exam_invigilator/settings.py
+++ exam_invigilator/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'exam_invigilator', # Database name
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'azmiya22',
         'HOST': 'localhost',   # Or an IP Address that your DB is hosted on
         'PORT': '3306',
     }
