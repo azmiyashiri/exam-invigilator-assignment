f964f9520315dfc19532e6cc893330d3801b6452
Ayeshath Azmiya shiri <162473054+azmiyashiri@users.noreply.github.com>
2024-09-03 19:34:32 +0530

    Update settings.py

--- exam_invigilator/settings.py
+++ exam_invigilator/settings.py
@@ -133,9 +133,3 @@ MEDIA_URL='/images/'
 STATIC_URL = '/static/'
 
 
-
-EMAIL_USE_TLS=True
-EMAIL_HOST='smtp.gmail.com'
-EMAIL_PORT=587
-EMAIL_HOST_USER='azmiyashiri22@gmail.com'
-EMAIL_HOST_PASSWORD='ilovemymom2infinity'
